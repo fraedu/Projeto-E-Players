5090e62a5c267eace0d2d68fd853f35bd8dd45d8
eduardob1609 <fraedu3@gmail.com>
2022-06-16 19:34:30 -0300

    Adicionando carrossel com Jquer

--- js/script.js
+++ js/script.js
@@ -1,3 +1,28 @@
+
+$(document).ready (function() {
+    let slideAtual = 1;
+    let listaSlides = ["banner_f1", "banner_cs", "banner_lol"]
+    setInterval (mudarSlide, 2500)
+
+    function mudarSlide() {
+        console.log ("Slide atual: ", slideAtual);
+        
+        if (slideAtual > 0) {
+            $("#carrossel").removeClass(listaSlides[slideAtual -1]) 
+        } else if (slideAtual == 0) {
+            $("#carrossel").removeClass (listaSlides[ (listaSlides.length) -1 ])
+        }
+        
+        $("#carrossel").addClass (listaSlides[slideAtual]);
+
+        slideAtual++
+
+        if (slideAtual > (listaSlides.length-1) ) {
+            slideAtual = 0;
+        }
+    }
+}) 
+
 function mostrarMenu() {
     let menu = document.getElementById ("menu")
     if (getComputedStyle(menu).display == 'none') {
@@ -5,4 +30,9 @@ function mostrarMenu() {
     } else {
         menu.style.display = 'none';
     }
+}
+
+function cadastrarNewsLetter() {
+    let email = document.getElementById ("campo-email") .value;
+    console.log ("email")
 }
